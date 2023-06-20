adc92579beb870633d8d6a230e56127a51499635
NyTakoeSebe <evd.matvey@mail.ru>
2023-06-20 20:59:50 +0700

    test

--- js/script.js
+++ js/script.js
@@ -3,23 +3,27 @@ const productSlider = new Swiper('.product__slider', {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 20,
-  // breakpoints: {
-  //   970: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 20,
-  //   },
-  //   770: {
-  //     slidesPerView: 2.5,
-  //   },
-  //   620: {
-  //     slidesPerView: 2,
-  //   },
-  //   300: {
-  //     slidesPerView: 1,
-  //   },
-  // },
   navigation: {
     nextEl: '.product__slider-next',
     prevEl: '.product__slider-prev',
+  },
+});
+
+const reviewsSlider = new Swiper('.reviews__slider', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 2,
+  breakpoints: {
+    960: {
+      slidesPerView: 2,
+    },
+    320: {
+      slidesPerView: 1,
+    },
+  },
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.reviews__slider-next',
+    prevEl: '.reviews__slider-prev',
   },
 });
